d8f1483872dcaae76760ce11c80f73d1c65eb347
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-02-23 18:14:26 +0200

    Install and register angularfire & firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
@@ -16,35 +20,38 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BsNavbarComponent,
-    HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
-    AdminProductsComponent,
-    AdminOrdersComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule.forRoot(),
-    RouterModule.forRoot([
-        { path: '', component: HomeComponent },
-        { path: 'products', component: ProductsComponent },
-        { path: 'shopping-cart', component: ShoppingCartComponent },
-        { path: 'check-out', component: CheckOutComponent },
-        { path: 'order-success', component: OrderSuccessComponent },
-        { path: 'my/orders', component: MyOrdersComponent },
-        { path: 'login' , component: LoginComponent },
-        { path: 'admin/products', component: AdminProductsComponent },
-        { path: 'admin/orders', component: AdminOrdersComponent }
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BsNavbarComponent,
+        HomeComponent,
+        ProductsComponent,
+        ShoppingCartComponent,
+        CheckOutComponent,
+        OrderSuccessComponent,
+        MyOrdersComponent,
+        AdminProductsComponent,
+        AdminOrdersComponent,
+        LoginComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        NgbModule.forRoot(),
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent },
+            { path: 'products', component: ProductsComponent },
+            { path: 'shopping-cart', component: ShoppingCartComponent },
+            { path: 'check-out', component: CheckOutComponent },
+            { path: 'order-success', component: OrderSuccessComponent },
+            { path: 'my/orders', component: MyOrdersComponent },
+            { path: 'login' , component: LoginComponent },
+            { path: 'admin/products', component: AdminProductsComponent },
+            { path: 'admin/orders', component: AdminOrdersComponent }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
