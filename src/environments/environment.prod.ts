63dcd4fca9155edce845f0490784211f2045cd7e
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-02-23 18:15:05 +0200

    Add firebase to the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyA65vZDcLbcUMKi09wnpkTEmLe6CfJnTtE',
+        authDomain: 'e-shop-d88e6.firebaseapp.com',
+        databaseURL: 'https://e-shop-d88e6.firebaseio.com',
+        projectId: 'e-shop-d88e6',
+        storageBucket: 'e-shop-d88e6.appspot.com',
+        messagingSenderId: '572074028192'
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyA65vZDcLbcUMKi09wnpkTEmLe6CfJnTtE',
+        authDomain: 'e-shop-d88e6.firebaseapp.com',
+        databaseURL: 'https://e-shop-d88e6.firebaseio.com',
+        projectId: 'e-shop-d88e6',
+        storageBucket: 'e-shop-d88e6.appspot.com',
+        messagingSenderId: '572074028192'
+    }
 };
