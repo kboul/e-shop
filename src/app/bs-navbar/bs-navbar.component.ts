f4470d5f2fadbc9485339cd16f7625d4b396f32c
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-02-23 18:39:08 +0200

    Display the current user, remove login option in navbar after login, implement logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'bs-navbar',
     templateUrl: './bs-navbar.component.html',
     styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-    constructor() { }
+    user;
 
-    ngOnInit() {
+    constructor(private afAuth: AngularFireAuth) {
+        afAuth.authState.subscribe((user) => {
+            console.log(user);
+            this.user = user;
+        });
+    }
+
+    logout() {
+        this.afAuth.auth.signOut();
     }
 
 }
