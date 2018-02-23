89e23b9c45815bdf4e7d2925a6bbe7aed8f6721e
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-02-23 16:58:59 +0200

    Defining the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -25,10 +26,21 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
     OrderSuccessComponent,
     MyOrdersComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: '', component: HomeComponent },
+        { path: 'products', component: ProductsComponent },
+        { path: 'shopping-cart', component: ShoppingCartComponent },
+        { path: 'check-out', component: CheckOutComponent },
+        { path: 'order-success', component: OrderSuccessComponent },
+        { path: 'login' , component: LoginComponent },
+        { path: 'admin/products', component: AdminProductsComponent },
+        { path: 'admin/orders', component: AdminOrdersComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
