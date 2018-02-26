6954b46581fc7a893b312321958630544ae1712e
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-02-26 15:34:03 +0200

    Redirect to login page after logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './../services/auth.service';
 import { Component } from '@angular/core';
 import { AppUser } from '../models/app-user';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'bs-navbar',
@@ -10,11 +11,14 @@ import { AppUser } from '../models/app-user';
 export class BsNavbarComponent {
     appUser: AppUser;
 
-    constructor(private auth: AuthService) {
+    constructor(
+        private auth: AuthService,
+        private router: Router) {
         auth.appUser$.subscribe(appUser => this.appUser = appUser);
     }
 
     logout() {
        this.auth.logout();
+       this.router.navigate(['/login']);
     }
 }
