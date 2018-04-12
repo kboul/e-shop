706e4a665367451dba72426722be743bd597c372
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-04-12 10:56:49 +0300

    Filter products by category

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,7 @@
+import { Product } from './../../models/product';
 import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { ProductService } from './../product-form/product.service';
 import { Subscription } from 'rxjs/Subscription';
-import { Product } from './product';
 import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,5 @@
+import { Product } from './../models/product';
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../services/category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../admin/product-form/product.service';
@@ -8,16 +10,31 @@ import { ProductService } from '../admin/product-form/product.service';
     styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-    products$;
+    // initialize arrays to avoid getting undefined in the first place
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
     categories$;
+    category: string;
 
     constructor(
         private productService: ProductService,
-        private categoryService: CategoryService) { }
+        private categoryService: CategoryService,
+        private actibatedRoute: ActivatedRoute) { }
 
     ngOnInit() {
-        this.products$ = this.productService.getProducts();
+        this.productService.getProducts().subscribe(products =>  {
+            // get all the products when first loading the page
+            this.filteredProducts = this.products = products;
+        });
         this.categories$ = this.categoryService.getCategories();
+
+        // extract the selected category
+        this.actibatedRoute.queryParamMap.subscribe(params => {
+            this.category = params.get('category');
+
+            this.filteredProducts = (this.category) ?
+                this.products.filter(p => p.category === this.category) : this.products;
+        });
     }
 
 }
