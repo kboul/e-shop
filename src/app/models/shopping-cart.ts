b349dd604e11c502f48dcc8035c73a2e74c346a0
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-06-19 16:11:31 +0300

    Displaying the total price of the selected products in the cart

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -4,8 +4,17 @@ export class ShoppingCart {
     items: ShoppingCartItem[] = [];
 
     constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
-        for (const productId in itemsMap)
-            this.items.push(itemsMap[productId]);
+        for (const productId of Object.keys(itemsMap)) {
+            const item = itemsMap[productId];
+            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+        }
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (const productId in this.items)
+            sum +=  this.items[productId].totalPrice;
+        return sum;
     }
 
     get totalItemsCount() {
