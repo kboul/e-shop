ffcf470002de0db6efb3a21cd4acaac4facc0c3f
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-06-20 13:30:53 +0300

    Refactor products template - use async pipe & avoid subscription

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,24 +1,23 @@
-import { Subscription } from 'rxjs';
-import { Product } from './../models/product';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CategoryService } from './../services/category.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../admin/product-form/product.service';
 import { ShoppingCartService } from '../services/shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Observable } from 'rxjs';
+import { CategoryService } from './../services/category.service';
 
 @Component({
     selector: 'app-products',
     templateUrl: './products.component.html',
     styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit, OnDestroy {
+export class ProductsComponent implements OnInit {
     // initialize arrays to avoid getting undefined in the first place
     products = [];
     filteredProducts = [];
     categories$;
     category: string;
-    cart: any;
-    subscription: Subscription;
+    cart$: Observable<ShoppingCart>;
 
     constructor(
         private productService: ProductService,
@@ -41,12 +40,7 @@ export class ProductsComponent implements OnInit, OnDestroy {
                 this.products.filter(p => p.category === this.category) : this.products;
         });
 
-        this.subscription = (await this.cartService.getCart())
-            .subscribe(cart => this.cart = cart);
-    }
-
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
+        this.cart$ = await this.cartService.getCart();
     }
 }
 
